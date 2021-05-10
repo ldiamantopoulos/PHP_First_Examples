e5a6e68685b5d5351e7b7ebd4c5d96532968dff3
ldiamantopoulos <ldiamantopoulos@sqlearn.com>
2021-05-10 13:52:25 +0300

    pdo and other db connection

--- com.leo.learnPHPandMySQL.course/PDOConnectionToDB.php
+++ com.leo.learnPHPandMySQL.course/PDOConnectionToDB.php
@@ -1,13 +1,13 @@
 <?php
 //PDO is more flexible because you can use it with multiple types of databases
 // and more secure as it uses prepared statements
-$id = 2;
+$id = 1;
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=myDatabase', 'leo', 'password');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    $conn = new PDO('mysql:host=localhost;dbname=company;charset=utf8', 'root','12345');
+/   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare('SELECT * FROM employees WHERE id = :id');
-    $stmt->execute(array('id' => $id));
+   $stmt = $conn->prepare('SELECT * FROM employees WHERE id = :id');
+   $stmt->execute(array('id' => $id));
 
     while ($row = $stmt->fetch()) {
         print_r($row);
